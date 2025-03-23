70b8e55019400d1f46a07af69c2a3ebe1d814e8a
Mileto Almeida <almeidamileto@gmail.com>
2025-03-23 10:13:48 -0300

    :sparkles: feat: add support files and update SCSS imports

---
+++ src/HtmlGenerator.java
@@ -0,0 +1,16 @@
+package src;
+
+public class HtmlGenerator {
+    public void generateHtml(String title, String body) {
+        String head =
+                """
+                 <head>
+                   <meta charset="UTF-8">
+                   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                     <title>%s</title>
+                    </head>
+                    """;
+
+
+    }
+}
